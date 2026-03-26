ce7c8bbe19098f5b50cf2ffdd4f84bfef4bba1bd
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2026-03-25 18:46:04 -0700

    Fix Next 16 marketing revalidation

--- src/lib/marketing/cache.ts
+++ src/lib/marketing/cache.ts
@@ -5,5 +5,5 @@ import { revalidateTag } from "next/cache";
 export const MARKETING_SNAPSHOT_TAG = "marketing-snapshot";
 
 export function revalidateMarketingSnapshot() {
-  revalidateTag(MARKETING_SNAPSHOT_TAG);
+  revalidateTag(MARKETING_SNAPSHOT_TAG, "max");
 }
